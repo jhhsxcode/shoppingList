8abc0b2cbe17efee2988749fb9f0ca7579a834bd
Bob Brown <bob.brown@d214.org>
2021-03-30 14:48:40 -0500

    Added outlast and action

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textFIeld: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func whenButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
 }
 
